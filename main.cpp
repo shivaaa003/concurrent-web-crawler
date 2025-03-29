9fb76387a59a6dbf3ffdf288715b035ae880dace
shivaaa003 <shivamsaini00300@gmail.com>
2025-03-29 14:06:25 +0530

    adding Recursive function to extract links from HTML using Gumbo

--- main.cpp
+++ main.cpp
@@ -28,6 +28,23 @@ string fetchPageContent(const string& url) {
     return (res == CURLE_OK) ? pageContent : "";
 }
 
+// Recursive function to extract links from HTML using Gumbo
+void extractLinks(GumboNode* node, vector<string>& links) {
+    if (node->type != GUMBO_NODE_ELEMENT) return;
+
+    if (node->v.element.tag == GUMBO_TAG_A) {
+        GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href");
+        if (href && href->value) {
+            links.push_back(href->value);
+        }
+    }
+
+    const GumboVector* children = &node->v.element.children;
+    for (size_t i = 0; i < children->length; ++i) {
+        extractLinks(static_cast<GumboNode*>(children->data[i]), links);
+    }
+}
+
 // Function to parse HTML and extract links
 vector<string> getLinksFromHTML(const string& html) {
     vector<string> links;
